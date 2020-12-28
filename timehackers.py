1b4981175d38046205f00bc6c76b99115b9f3dad
andyjacko <wisdom.mcr@gmail.com>
2020-12-28 03:02:22 +0000

    Update timehackers.py
    Refactored some game text...removed comments...reworded story...

--- timehackers.py
+++ timehackers.py
@@ -1,14 +1,11 @@
-#import sleep function from time library
 from time import sleep
 
-#Declare Global variables
 pause_time = 2
 andy_alive = 1 
 michal_alive = 1
 lasharn_alive = 1
 saihar_alive = 1
 
-#Declare text variables used throughout game as graphics and storyline
 intro_txt = '''
  ::::::::::: ::::::::::: ::::    ::::  ::::::::::   :::    :::     :::      ::::::::  :::    ::: :::::::::: :::::::::   ::::::::
      :+:         :+:     +:+:+: :+:+:+ :+:          :+:    :+:   :+: :+:   :+:    :+: :+:   :+:  :+:        :+:    :+: :+:    :+:
@@ -64,14 +61,14 @@ room_2_txt = '''
  !  xxxxxxxxx    xxxxxxxx  xx x x    x xx  xx   xxxx    xxxxx     !
  !xxxx         xxxx       xxx x x x x x xx  xxxx    xxxxx     xxxx! 
  !---------x======================================x---------------!
- !.........|        \  code_nation_co.   /        |...............!
- !.........| (^)_____\__________________/_____(^) |..[ENTER]......!
- !.........|          \                /          |..[ PSW ]......!
- !.........|           +--------------+           |..       ......!
- !.........|           |              |           |.. [ x ] ......!
- !.........| (^)_______|____+====+____|_______(^) |.. [ x ] ......!
- !.........|           |    |....| x  |           |.. [ x ] ......!
- !.........|           |    |....|    |           |.. [ x ] ......!
+ !.........|        \                    /        |...............!
+ !.........| (^)_____\__________________/_____(^) |... [ENTER] ...!
+ !.........|          \                /          |... [ PSW ] ...!
+ !.........|           +--------------+           |...............!
+ !.........|           |              |           |...  [ x ]  ...!
+ !.........| (^)_______|____+====+____|_______(^) |...  [ x ]  ...!
+ !.........|           |    |....| x  |           |...  [ x ]  ...!
+ !.........|           |    |....|    |           |...  [ x ]  ...!
  !.........|           |    |....|    |           |...............!
  !.........| (^)_______/_-_-_-_-_-_-_-\_______(^) |...............!
  !.........|        /./                \.\        |...............!
@@ -247,47 +244,59 @@ kurt_txt = '''
       ==-----------::::::::::.:-=====----=-::-----:..-===+======---========+++=+++++********+==*****++:::    ...:=+==++++++++++
       ------------:::::::::...-===-:-===-----=:::--::-=++++++===--===++++*********######***+==**###++=:-..   ...:==-+++++++++++\n\n'''
 
-welcome_txt = "Welcome to Time Hackers,\nWhere you’ll complete a series of missions with your 4 friends, Andy, Michal, Lasharn and Saihar.\n\nOnce you reach the time machine at the end, this means you have won the game and will be able to travel back in time to save Kurt Cobain.\n\nNow that you’ve arrived at the corridor of doom, you must get past the swarm of tarantulas, the laser beams, the pool of piranhas and the pit of lava to succeed.\nHelp your friends answer each question in order to carry them along with you."
-challenge_1_txt = "Challenge 1:\nTo escape the pit of deadly tarantulas,\nAndy must step forward and give the answer to the following question.\nWho is the best superhero?.\n"
-challenge_2_txt = "Challenge 2:\nYou have reached a bolted door with a giant keypad.\nTo unlock the door and safely get Michal through with you, get ready to input the correct code.\nIf you fail to unlock the door, it will activate the laser beams which will destroy Michal.\nThere is a clue on the door which asks what year did Kurt Cobain die?\n"
+welcome_txt = "Welcome to Time Hackers,\nYou are about to complete a series of challenges with your four friends: Andy, Michal, Lasharn and Saihar.\n\nOnce you complete all the challenges you will reach a time machine. You will be able to travel back in time and save Kurt Cobain.\n\nYou now find yourselves at the beginning of the corridor of doom. To reach the time machine, you must pass through a swarm of tarantulas, a locked door with laser beams, a pool of piranhas and a pit of lava.\nHelp your friends answer each challenge question in order to save them from death.\n"
+challenge_1_txt = "Challenge 1:\nYou encounter a swarm of deadly tarantulas,\nAndy steps forward to answer the following question.\nWho is the best superhero?.\n"
+challenge_2_txt = "Challenge 2:\nYou have reached a locked door with a giant keypad.\nTo unlock the door and safely get Michal through with you, enter the correct code.\nIf you fail to unlock the door, it will activate the laser beams which will destroy Michal.\nThere is a clue on the door which asks what year did Kurt Cobain die?\n"
 challenge_3_txt = "Challenge 3:\nYou are now halfway through.\nThe next encounter is a pool of piranhas,\nLasharn must get ready to answer this next geographical question.\n"
-challenge_4_txt = "Challenge 4:\nYou’ve now reached the final mission, a pit full of molten lava.\nYou have an algebraic equation on the sign in front of you, What is the value of X?\nSaihar must answer the question correctly to proceed.\n"
-challenge_5_txt = "This is it!\nYou tried your best and made it to the time machine.\nAwesome!\n"
+challenge_4_txt = "Challenge 4:\nYou’ve now reached the final challenge, a pit full of molten lava.\nYou have an algebraic equation on a sign in front of you, What is the value of X?\nSaihar must answer the question correctly to proceed.\n"
+challenge_5_txt = "This is it!\nYou finally made it through all the challenges, and before you is the time machine.\nAwesome!\n"
 
-#All the text for right/wrong answers used throughout the game
-challenge_1_right = "RIGHT – GREAT! The tarantulas have climbed back into their webs, allowing you to safely pass through."
-challenge_1_wrong = "WRONG – OH NO! That was the wrong answer. Andy didn’t survive, but Michal, Lasharn and Saihar will continue."
-challenge_2_right = "RIGHT – AWESOME! Michal entered the code in time. Wait for the door to slide open and continue on your quest."
-challenge_2_wrong = "WRONG – OH NO! Michal didn’t get the code correct and now he is no more! The door will slide open to allow the others through."
-challenge_3_right = "RIGHT – CORRECT! The statement is false. A bridge rises out of the water so that the group can continue on."
-challenge_3_wrong = "WRONG – OH NO! Wrong answer, the highest mountain is actually Mount Everest. Lasharn has been attacked by the piranhas, the group continue on for the next stage."
-challenge_4_right = "RIGHT – WELL DONE! Saihar answered correctly. Wait for the stepping stones to appear and cross the lava lake together."
-challenge_4_wrong = "WRONG – OH NO! Incorrect answer. Saihar has been engulfed by the lava. Wait for the stepping stones to appear and cross the lava lake."
+challenge_1_quest = "Type \"A\" for Thor, \"B\" for Spiderman, \"C\" for Supergirl or \"D\" for Batman:\n"
+challenge_1_right = "\nRIGHT – GREAT! The tarantulas have all climbed back into their webs, allowing you to safely pass through.\n"
+challenge_1_wrong = "\nWRONG – OH NO! That was the wrong answer. Andy didn’t survive, but Michal, Lasharn and Saihar will continue.\n"
+challenge_2_quest = "Type the code to pass?\n"
+challenge_2_right = "\nRIGHT – AWESOME! Michal entered the code correctly. You wait for the door to slide open and continue on your quest.\n"
+challenge_2_wrong = "\nWRONG – OH NO! Michal didn’t get the code correct and now he vaprorised by laser beams! The door slides open to allow the others through.\n"
+challenge_3_quest = "Mount Kilimanjaro is the highest mountain in the world. True or False?\n"
+challenge_3_right = "\nRIGHT – CORRECT! The statement is false. It's Mount Everest. A bridge rises out of the water so that the group can continue.\n"
+challenge_3_wrong = "\nWRONG – OH NO! Wrong answer, the highest mountain is actually Mount Everest. Lasharn is attacked and eaten by the piranhas. A bridge rises out of the water so that the group can continue..\n"
+challenge_4_quest = "3x + 2 = 14\n"
+challenge_4_right = "\nRIGHT – WELL DONE! Saihar answered correctly. Stepping stones appear and allow the group to cross the lava lake together.\n"
+challenge_4_wrong = "\nWRONG – OH NO! Incorrect answer. Saihar has been engulfed by the lava. Stepping stones appear and you cross the lava lake.\n"
+challenge_5_quest = "You now have the option to travel back in time to save Kurt Cobain or go back and save your friend/s.\n\nEnter \"A\" if you wish to use the time machine to save Kurt or enter \"B\" to go back and rescue your friend/s\n"
 
-#Clear screen by printing it full of blank characters
+ending_1_txt = "You go back in time and save Kurt Cobain\nCONGRATS, YOU WIN THE GAME!"
+ending_2_txt = "CONGRATS, everyone survived! You and your friends use the time machine to save Kurt Cobain together.\n\n\n"
+
+credits_1_txt = "                                                C R E D I T S"
+credits_2_txt = "                                                  ANDY JACKO"
+credits_3_txt = "                                                MICHAL WERNER"
+credits_4_txt = "                                                  LASHARN F"
+credits_5_txt = "                                                SAIHAR SIDDIQUE"
+credits_6_txt = "                                                 T H E  E N D"
+
+new_line = "\n"
 clear_screen = "\033[H\033[J"
 
-#Create function to print text 1 char at a time...
 def type_slow(print_this):
-  for char in print_this:              #For every character in string 
-    sleep(0.03)                        #Pause for a little bit
-    print(char, end='', flush=True)    #Print character then flush the i/o buffer so it can print the next character
+  for char in print_this:
+    sleep(0.03)
+    print(char, end="", flush=True)
 
-#Function to loop through credits line by line displaying names and ASCII art. Not very well, but you know, we didnt have a Lucasfilm budget.
 def roll_credits():
   for i in range(80):
     sleep(0.25)
-    print(" \n")
+    print(new_line)
     if i == 10:
-      print("                                                C R E D I T S")
+      print(credits_1_txt)
     if i == 15:
-      print("                                                  ANDY JACKO")
+      print(credits_2_txt)
     if i == 45:
-      print("                                                MICHAL WERNER")
+      print(credits_3_txt)
     if i == 25:
-      print("                                                  LASHARN F")
+      print(credits_4_txt)
     if i == 35:
-      print("                                                SAIHAR SIDDIQUE")
+      print(credits_5_txt)
     if i == 30:  
       print(spider_small_txt)
     if i == 40:  
@@ -297,15 +306,9 @@ def roll_credits():
     if i == 70:  
       print(kurt_txt)
     if i == 60:
-      print("                                                 T H E  E N D")
+      print(credits_6_txt)
 
-#Function to start and restart the game resetting global variables back to being alive on restart
 def start_game():
-  print(clear_screen)
-  print(intro_txt)
-  type_slow(welcome_txt)
-  print("\n")
-  #Make everyone alive incase game has been restarted
   global andy_alive
   andy_alive = 1
   global michal_alive
@@ -315,165 +318,147 @@ def start_game():
   global saihar_alive
   saihar_alive = 1
 
-  sleep(pause_time)  #Pause for a little bit
-  room_1() #Move to room 1
+  print(clear_screen)
+  print(intro_txt)
+  type_slow(welcome_txt)
+  
+  sleep(pause_time * 2)
+  room_1()
 
-#Function for the first challenge room
 def room_1():
   print(clear_screen)
   print(room_1_txt)
-  type_slow(challenge_1_txt)
-  andy_question = input("\nType A for Thor, B for Spiderman, C for Supergirl or D for Batman:\n")
+  type_slow(challenge_1_txt + new_line)
+  andy_question = input(challenge_1_quest)
   
   while andy_question.lower() != "a" and andy_question.lower() != "b" and andy_question.lower() != "c" and andy_question.lower() != "d": 
     print(clear_screen)    
-    print(room_1_txt)                #Keep asking unless a, b, c or d entered
+    print(room_1_txt)
     print(challenge_1_txt)
-    andy_question = input("Type A for Thor, B for Spiderman, C for Supergirl or D for Batman:\n")
+    andy_question = input(challenge_1_quest)
   
   if andy_question.lower() != "b":
     global andy_alive
     andy_alive = 0
-    print("\n")
     type_slow(challenge_1_wrong)
-    print("\n")
   else:
-    print("\n")
     type_slow(challenge_1_right)
-    print("\n")
 
-  sleep(pause_time)  #Pause for a little bit
-  room_2() #Move to room 2
+  sleep(pause_time)
+  room_2()
 
-#Function for the second challenge room
 def room_2():  
   print(clear_screen)
   print(room_2_txt)
-  type_slow(challenge_2_txt)
-  michal_question = input("\nType the code to pass? \n")
+  type_slow(challenge_2_txt + new_line)
+  michal_question = input(challenge_2_quest)
   
-  while michal_question == "": #Keep asking if nothing is entered but return pressed
+  while michal_question == "":
     print(clear_screen)
     print(room_2_txt)
     print(challenge_2_txt)
-    michal_question = input("Type the code to pass? \n")
+    michal_question = input(challenge_2_quest)
   
   if michal_question.lower() != "1994":
     global michal_alive
     michal_alive = 0
-    print("\n")
     type_slow(challenge_2_wrong)
-    print("\n")
   else:
-    print("\n")
     type_slow(challenge_2_right)
-    print("\n")
 
-  sleep(pause_time)  #Pause for a little bit
-  room_3() #Move to room 3
+  sleep(pause_time)
+  room_3()
 
-#Function for the third challenge room
 def room_3():
   print(clear_screen)  
   print(room_3_txt)
-  type_slow(challenge_3_txt)
-  lasharn_question = input("\nMount Kilimanjaro is the highest mountain in the world. True or False? \n")
+  type_slow(challenge_3_txt + new_line)
+  lasharn_question = input(challenge_3_quest)
   
-  while lasharn_question.lower() != "true" and lasharn_question.lower() != "false": #Keep asking until either true or false entered
+  while lasharn_question.lower() != "true" and lasharn_question.lower() != "false":
     print(clear_screen)
     print(room_3_txt)
     print(challenge_3_txt)
-    lasharn_question = input("Mount Kilimanjaro is the highest mountain in the world. True or False? \n")    
+    lasharn_question = input(challenge_3_quest)    
     
   if lasharn_question.lower() != "false":
     global lasharn_alive
     lasharn_alive = 0
-    print("\n")
     type_slow(challenge_3_wrong)
-    print("\n")
   else:
-    print("\n")
     type_slow(challenge_3_right)
-    print("\n")
 
-  sleep(pause_time) #Pause for a little bit
-  room_4() #Move to room 4
+  sleep(pause_time)
+  room_4()
  
-#Function for the fourth challenge room
 def room_4():
   print(clear_screen)
   print(room_4_txt)  
-  type_slow(challenge_4_txt)
-  saihar_question = input("\n3x + 2 = 14\n")
+  type_slow(challenge_4_txt + new_line)
+  saihar_question = input(challenge_4_quest)
   
-  while saihar_question == "": #Keep asking if nothing is entered but return pressed
+  while saihar_question == "":
     print(clear_screen)
     print(room_4_txt)   
     print(challenge_4_txt)
-    saihar_question = input("3x + 2 = 14 \n")
+    saihar_question = input(challenge_4_quest)
   
   if saihar_question.lower() != "4":
     global saihar_alive
     saihar_alive = 0
-    print("\n")
     type_slow(challenge_4_wrong)
-    print("\n")
   else:
-    print("\n")
     type_slow(challenge_4_right)
-    print("\n")
 
-  sleep(pause_time)  #Pause for a little bit
-  room_5() #Move to final room
+  sleep(pause_time)
+  room_5()
 
-#Function for the final room
 def room_5():
   print(clear_screen)  
   print(time_machine_txt)
-  type_slow(challenge_5_txt)
-  print("\n")
-  #Check if any players died. If they did you get 1 ending if they didnt you get a completely different ending. WOW!
+  type_slow(challenge_5_txt + new_line)
+  
   if andy_alive == 0 or michal_alive == 0 or lasharn_alive == 0 or saihar_alive == 0 :
-    dead_players = ["DEAD"] #Create list of dead playser entering a word to take up index 0
+    dead_players = ["DEAD"]
     if andy_alive == 0:
       dead_players.append("Andy")
     if michal_alive == 0:
-      dead_players.append("Michal")                   #If anyone died add to the list
+      dead_players.append("Michal")
     if lasharn_alive == 0:
       dead_players.append("Lasharn")
     if saihar_alive == 0:
       dead_players.append("Saihar")
-    if len(dead_players) > 2: #If greater than 2 means more than 1 person died so use plural
+    if len(dead_players) > 2:
       d_p = "Your friends "
     else:
       d_p = "Your friend "    
     
     if len(dead_players) > 1:
-      for i in range(len(dead_players)):   #Loop through dead list putting name comma and space (, )in the string
+      for i in range(len(dead_players)):
         if i > 0:
           d_p += dead_players[i] + ", "
           
-    d_p = d_p[:-2] #Remove the ", " from the end of the string
-    d_p = d_p + " died." #Add to end of string forming full sentence
+    d_p = d_p[:-2]
+    d_p = d_p + " died."
     print(d_p)
-    all_question = input("\nYou have the option to travel back in time to save Kurt Cobain or restart the mission and revive your friend/s.\n\nEnter A if you wish to use the time machine to save Kurt or enter B to go back and rescue your friend/s\n")
+    all_question = input(challenge_5_quest)
 
-    while all_question.lower() != "a" and all_question.lower() != "b": #Keep asking unless the answer is a or b
+    while all_question.lower() != "a" and all_question.lower() != "b":
       print(clear_screen)
       print(time_machine_txt)
       print(challenge_5_txt)
       print(d_p)
-      all_question = input("You have the option to travel back in time to save Kurt Cobain or restart the mission and revive your friend/s.\n\nEnter A if you wish to use the time machine to save Kurt or enter B to go back and rescue your friend/s\n")    
+      all_question = input(challenge_5_quest)    
     
-    if all_question.lower() == "a": #If answer is a go and save Kurt
+    if all_question.lower() == "a":
       print(clear_screen)
       print(time_machine_txt)
-      type_slow("You go back in time and save Kurt Cobain\nCONGRATS, YOU WIN THE GAME!")
-      roll_credits() #Does what it says on the tin
+      type_slow(ending_1_txt)
+      roll_credits()
     else: 
-      start_game() #This 1 resets the game if you choose to save your friends
+      start_game()
   else:
-    type_slow("CONGRATS, everyone survived! You and your friends can use the time machine to save Kurt Cobain together.\n\n\n")
-    roll_credits() #Does what it says on the tin
+    type_slow(ending_2_txt)
+    roll_credits()
 
-start_game() #This 1 line right here starts the whole thing going
+start_game()
